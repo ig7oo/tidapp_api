ed48c34ddd8069390569d621603b2f64ff4cad43
ig7oo <davidwesterlund012@gmail.com>
2024-01-16 13:19:56 +0200

    Hämta alla aktiviteter fungerar

--- test/TestActivities.php
+++ test/TestActivities.php
@@ -26,6 +26,13 @@ function allaActivityTester(): string {
 function test_HamtaAllaAktiviteter(): string {
     $retur = "<h2>test_HamtaAllaAktiviteter</h2>";
     try {
+        $svar=hamtaAllaAktiviteter();
+        if($svar->getStatus() === 200){
+            $retur.= "<p class='ok'>Hämta alla aktiviteter lyckades". count($svar->getContent())
+            . " poster returnerades</p>";
+        } else {
+            $retur.= "<p class='error'>Hämta alla aktiviteter misslyckades<br>" .$svar->getStatus(). "returnerades</p>";
+        }
         $retur .= "<p class='error'>Inga tester implementerade</p>";
     } catch (Exception $ex) {
         $retur .= "<p class='error'>Något gick fel, meddelandet säger:<br> {$ex->getMessage()}</p>";
